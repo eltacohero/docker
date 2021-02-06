1fb00115f61c94cad1e9aa1873b70677d9376cac
eltacohero <jrm.grondin@protonmail.com>
2021-02-06 10:50:29 +0100

    update

---
+++ python/date.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+import time
+
+while True:
+    fname = '/shared/index.html'
+    with open(fname, 'w') as f:
+        now = datetime.now()
+        f.write(now.strftime("%H:%M:%S on %A, %B the %dth, %Y"))
+    time.sleep(3600)
